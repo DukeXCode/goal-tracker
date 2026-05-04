3e3fb97c021b5af5e00cdf5b5921fff287c09a32
duke <naim.giger@outlook.com>
2026-05-04 10:40:03 +0200

    Remove unneeded import

--- apps/web/src/hooks/useGoals.ts
+++ apps/web/src/hooks/useGoals.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import type { Goal, GoalStatus, CreateGoalInput, UpdateGoalInput } from "@goal-tracker/shared";
 import { api } from "../lib/api";
 
